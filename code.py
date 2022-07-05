d7e153e018a1f088cb7376914d8aecbbd8cf63de
MahiVarshni <84715191+MahiVarshni@users.noreply.github.com>
2022-07-05 19:22:25 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,31 @@
+import cv2  
+import numpy as np  
+  
+video = cv2.VideoCapture(0) 
+image = cv2.imread("0aa373d1ed704c41ede1a6bc80056356f6c343c9f77d658acb656d02b91d4751._RI_.jpg") 
+  
+while True: 
+  
+    ret, frame = video.read() 
+    print(frame)
+    frame = cv2.resize(frame, (640, 480)) 
+    image = cv2.resize(image, (640, 480)) 
+  
+  
+    u_black = np.array([104, 153, 70]) 
+    l_black = np.array([30, 30, 0]) 
+  
+    mask = cv2.inRange(frame, l_black, u_black) 
+    res = cv2.bitwise_and(frame, frame, mask = mask) 
+  
+    f = frame - res 
+    f = np.where(f == 0, image, f) 
+  
+    cv2.imshow("video", frame) 
+    cv2.imshow("mask", f) 
+  
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+  
+video.release() 
+cv2.destroyAllWindows() 
